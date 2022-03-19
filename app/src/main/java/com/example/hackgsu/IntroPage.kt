23c9a04f995dbceac3fd98e28146e3aa3eb2d8b7
Tushar <thedynamic2508@gmail.com>
2022-03-19 03:10:09 -0400

    Devp-GSU_Hacks

---
+++ app/src/main/java/com/example/hackgsu/IntroPage.kt
@@ -0,0 +1,11 @@
+package com.example.hackgsu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IntroPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro_page)
+    }
+}
--- app/src/main/java/com/example/hackgsu/Splash.kt
+++ app/src/main/java/com/example/hackgsu/Splash.kt
@@ -6,12 +6,17 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 
-class MainActivity : AppCompatActivity() {
+class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
         supportActionBar?.hide() //byRajul
-
-
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed(
+            {
+                val intent = Intent(this,MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            },3000)
     }
 }
