154b6757d8d08f5ef475af2731f2e9eb88ab801e
Tushar <thedynamic2508@gmail.com>
2022-03-19 11:45:49 -0400

    BegDone1

--- app/src/main/java/com/example/hackgsu/SignInPage.kt
+++ app/src/main/java/com/example/hackgsu/SignInPage.kt
@@ -7,5 +7,6 @@ class SignInPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in_page)
+        supportActionBar?.hide() //byRajul
     }
 }
